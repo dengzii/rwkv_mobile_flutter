569a9b32323d779bbb7afbf058df2ef242fa2fc3
WangCe <halowang1991@gmail.com>
2025-05-22 12:03:30 +0800

    reasoning button config

---
+++ example/lib/widgets/chat/reasoning_option_button.dart
@@ -0,0 +1,135 @@
+// ignore: unused_import
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gaimon/gaimon.dart';
+import 'package:halo/halo.dart';
+import 'package:halo_alert/halo_alert.dart';
+import 'package:halo_state/halo_state.dart';
+import 'package:zone/gen/l10n.dart';
+import 'package:zone/state/p.dart';
+
+enum ReasoningOption {
+  language,
+  pseudo,
+  ;
+
+  IconData get icon => switch (this) {
+        ReasoningOption.language => Icons.translate,
+        ReasoningOption.pseudo => Icons.bolt,
+      };
+}
+
+class ReasoningOptionButton extends ConsumerWidget {
+  final ReasoningOption option;
+
+  const ReasoningOptionButton({
+    super.key,
+    required this.option,
+  });
+
+  void _onTap() async {
+    final loading = P.rwkv.loading.q;
+    if (loading) {
+      Alert.info(S.current.please_wait_for_the_model_to_load);
+      return;
+    }
+    final receiving = P.chat.receivingTokens.q;
+    if (receiving) {
+      Alert.info(S.current.please_wait_for_the_model_to_finish_generating);
+      return;
+    }
+    final currentModel = P.rwkv.currentModel.q;
+    if (currentModel == null) {
+      if (P.fileManager.modelSelectorShown.q) return;
+      P.fileManager.modelSelectorShown.q = true;
+      return;
+    }
+
+    switch (option) {
+      case ReasoningOption.language:
+        final newValue = !P.rwkv.preferChinese.q;
+        await P.rwkv.setModelConfig(preferChinese: newValue);
+
+        if (newValue) Alert.success(S.current.prefer_chinese);
+        break;
+      case ReasoningOption.pseudo:
+        final newValue = !P.rwkv.preferPseudo.q;
+        await P.rwkv.setModelConfig(preferPseudo: newValue);
+        if (newValue) Alert.success("开启伪推理");
+        break;
+    }
+
+    Gaimon.light();
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final s = S.of(context);
+    final primary = Theme.of(context).colorScheme.primary;
+    final preferChinese = ref.watch(P.rwkv.preferChinese);
+    final preferPseudo = ref.watch(P.rwkv.preferPseudo);
+    final loading = ref.watch(P.rwkv.loading);
+
+    final iconColor = switch (option) {
+      ReasoningOption.language => preferChinese ? kW : kB.q(.25),
+      ReasoningOption.pseudo => preferPseudo ? kW : kB.q(.25),
+    };
+
+    final buttonColor = switch (option) {
+      ReasoningOption.language => preferChinese ? primary : kC,
+      ReasoningOption.pseudo => preferPseudo ? primary : kC,
+    };
+
+    final buttonBorderColor = switch (option) {
+      ReasoningOption.language => preferChinese ? primary.q(.5) : kB.q(.25),
+      ReasoningOption.pseudo => preferPseudo ? primary.q(.5) : kB.q(.25),
+    };
+
+    final contentPadding = switch (option) {
+      ReasoningOption.language => preferChinese ? const EI.o(l: 4, r: 8, t: 4, b: 4) : const EI.o(l: 4, r: 8, t: 4, b: 4),
+      ReasoningOption.pseudo => preferPseudo ? const EI.o(l: 4, r: 8, t: 4, b: 4) : const EI.o(l: 4, r: 4, t: 4, b: 4),
+    };
+
+    return AnimatedOpacity(
+      opacity: loading ? .33 : 1,
+      duration: 250.ms,
+      child: GD(
+        onTap: _onTap,
+        child: C(
+          decoration: BD(
+            color: buttonColor,
+            border: Border.all(color: buttonBorderColor),
+            borderRadius: 12.r,
+          ),
+          padding: contentPadding,
+          child: Ro(
+            c: CAA.center,
+            children: [
+              Icon(
+                option.icon,
+                color: iconColor,
+              ),
+              2.w,
+              Co(
+                c: CAA.start,
+                m: MAA.center,
+                children: [
+                  if (option == ReasoningOption.language) ...[
+                    if (preferChinese) T(s.prefer, s: const TS(c: kW, s: 10, height: 1)),
+                    if (preferChinese) T(s.chinese, s: const TS(c: kW, s: 10, height: 1)),
+                    if (!preferChinese && !preferPseudo) T(s.auto, s: TS(c: kB.q(.25), s: 10, height: 1)),
+                  ] else if (option == ReasoningOption.pseudo) ...[
+                    if (preferPseudo) T(s.prefer, s: const TS(c: kW, s: 10, height: 1)),
+                    if (preferPseudo) const T("伪推理", s: TS(c: kW, s: 10, height: 1)),
+                  ] else
+                    ...[]
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -9,6 +9,7 @@
 #include <audioplayers_windows/audioplayers_windows_plugin.h>
 #include <dynamic_color/dynamic_color_plugin_c_api.h>
 #include <file_selector_windows/file_selector_windows.h>
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <record_windows/record_windows_plugin_c_api.h>
 #include <sentry_flutter/sentry_flutter_plugin.h>
 #include <share_plus/share_plus_windows_plugin_c_api.h>
@@ -22,6 +23,8 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("DynamicColorPluginCApi"));
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
   RecordWindowsPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("RecordWindowsPluginCApi"));
   SentryFlutterPluginRegisterWithRegistrar(
